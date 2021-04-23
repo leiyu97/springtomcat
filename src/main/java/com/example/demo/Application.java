01fcae16fcc9dd85970f52d2b911523338f14220
Your Name <you@example.com>
2021-04-23 13:19:50 +0100

    add more

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -4,16 +4,25 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Properties;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-@PropertySource("file:///${CONFIG_LOCATION}/deploy.properties")
+@PropertySource("file:${CONFIG_LOCATION}/deploy.properties")
 public class Application {
+
+    @Value("${CONFIG_LOCATION}")
+    private String configLocation;
+
+    @Autowired
+    private Environment env;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
@@ -24,6 +33,7 @@ public class Application {
         return args -> {
 
             System.out.println("Let's inspect the beans provided by Spring Boot:");
+            System.out.println("Config location: " + configLocation);
 
             String[] beanNames = ctx.getBeanDefinitionNames();
             Arrays.sort(beanNames);
@@ -32,15 +42,17 @@ public class Application {
             }
 
             printWelcome();
-            readFromPropertiesFile();
+            //readFromPropertiesFile();
         };
     }
 
 
-    public void readFromPropertiesFile() {
+    /*public void readFromPropertiesFile() {
         Properties prop = new Properties();
         try {
-            prop.load(Application.class.getClassLoader().getResourceAsStream("deploy.properties"));
+
+            String propsLocation = System.getenv("CONFIG_LOCATION");
+           // prop.load(Application.class.getClassLoader().getResourceAsStream("deploy.properties"));
 
             System.out.println("**** properties content is "+ prop.getProperty("test"));
 
@@ -48,10 +60,11 @@ public class Application {
             ioException.printStackTrace();
         }
 
-    }
+    }*/
 
     public  void printWelcome() {
         System.out.println("This is to check if the build is taking the new code");
+        System.out.println("****"+ env.getProperty("test"));
     }
 
 }
